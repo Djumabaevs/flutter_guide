2821bc10343225378795811bbcfc548fcabd96d5
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-01 00:17:59 +0600

    app bar from scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello Bakyt!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Text('Body text'),
+      ),
     );
   }
 }
