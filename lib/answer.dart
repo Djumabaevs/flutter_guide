8a33c4b4bcf5fb3b6de50dda2c1263fac4547b33
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-12 00:27:39 +0600

    callback function

--- lib/answer.dart
+++ lib/answer.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: RaisedButton(
+        child: Text('Answer 1'),
+        onPressed: answerQuestion,
+      ),
+    );
   }
 }
